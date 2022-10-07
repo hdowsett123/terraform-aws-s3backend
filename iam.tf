573e842233785289b44e03533962de97195e7ad1
hdowsett123 <harrydowsett@googlemail.com>
2022-10-07 07:42:57 +0100

    Fixed 'space' error on 'EOF' code.

--- iam.tf
+++ iam.tf
@@ -1,12 +1,13 @@
 data "aws_caller_identity" "current" {}
  
-locals { 
-  principal_arns = var.principal_arns != null ? var.principal_arns : [data.aws_caller_identity.current.arn] 
+locals {
+  principal_arns = var.principal_arns != null ? var.principal_arns : [data.aws_caller_identity.current.arn]  
 }
 
 resource "aws_iam_role" "iam_role" { 
   name = "${local.namespace}-tf-assume-role"
-  assume_role_policy = <<-EOF 
+
+  assume_role_policy = <<-EOF
     { 
       "Version": "2012-10-17", 
       "Statement": [ 
@@ -18,8 +19,8 @@ resource "aws_iam_role" "iam_role" {
           "Effect": "Allow" 
         } 
       ] 
-    } 
-  EOF 
+    }
+  EOF
   tags = { 
     ResourceGroup = local.namespace 
   } 
